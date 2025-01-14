386298a2313b3fdb6b193f7087b96f5f4d6750d0
ArianFrutoss <arian.frutos@ikasle.aeg.eus>
2025-01-14 12:46:43 +0100

    ADD - the same tests for calm elixires

--- test/elixir.test.js
+++ test/elixir.test.js
@@ -151,4 +151,152 @@ describe('Cuando el numero de ingredientes es 2-4', () => {
             });
         });
     });
+    describe('Cuando los efectos de ingredientes asociados llevaran los nombres: "Calm"', () => {
+        describe('Cuando todos los ingredientes tienen el mismo atributo (INT, DEX...)', () => {
+            describe('Cuando todos los efectos son del tipo least', () => {
+                it('El valor resultante del atributo sera la media de los values de los ingredientes. Una vez calculada la media se redondeara al multiplo de 5 inferior', async () => {
+
+                    const ingredients = await getIngredients();
+                    const diseases = await getDiseases();
+
+                    const selectedIngredients = [getSelectedIngredient(ingredients, "Quieting Root"), getSelectedIngredient(ingredients, "Quieting Root")];
+
+                    const elixir = PotionFactory.createPotion(selectedIngredients, diseases);
+
+                    expect(elixir.modifiers.charisma).toBe(-5);
+                });
+                it('La duracion sera la media de duraciones de los efectos de cada ingrediente, redondeada para abajo', async () => {
+
+                    const ingredients = await getIngredients();
+                    const diseases = await getDiseases();
+
+                    const selectedIngredients = [getSelectedIngredient(ingredients, "Quieting Root"), getSelectedIngredient(ingredients, "Quieting Root")];
+
+                    const elixir = PotionFactory.createPotion(selectedIngredients, diseases);
+
+                    expect(elixir.duration).toBe(1);
+                });
+                describe('Cuando todos los efectos son del tipo lesser', () => {
+                    it('El valor resultante del atributo sera la media de los values de los ingredientes. Una vez calculada la media se redondeara al multiplo de 5 inferior', async () => {
+    
+                        const ingredients = await getIngredients();
+                        const diseases = await getDiseases();
+    
+                        const selectedIngredients = [getSelectedIngredient(ingredients, "Peaceful Herb"), getSelectedIngredient(ingredients, "Peaceful Herb")];
+    
+                        const elixir = PotionFactory.createPotion(selectedIngredients, diseases);
+    
+                        expect(elixir.modifiers.charisma).toBe(-10);
+                    });
+                    it('La duracion sera la media de duraciones de los efectos de cada ingrediente, redondeada para abajo', async () => {
+    
+                        const ingredients = await getIngredients();
+                        const diseases = await getDiseases();
+    
+                        const selectedIngredients = [getSelectedIngredient(ingredients, "Peaceful Herb"), getSelectedIngredient(ingredients, "Peaceful Herb")];
+    
+                        const elixir = PotionFactory.createPotion(selectedIngredients, diseases);
+    
+                        expect(elixir.duration).toBe(1);
+                    });
+                });
+                describe('Cuando todos los efectos son del tipo normal', () => {
+                    it('El valor resultante del atributo sera la media de los values de los ingredientes. Una vez calculada la media se redondeara al multiplo de 5 inferior', async () => {
+    
+                        const ingredients = await getIngredients();
+                        const diseases = await getDiseases();
+    
+                        const selectedIngredients = [getSelectedIngredient(ingredients, "Tranquil Leaf"), getSelectedIngredient(ingredients, "Tranquil Leaf")];
+    
+                        const elixir = PotionFactory.createPotion(selectedIngredients, diseases);
+
+                        expect(elixir.modifiers.charisma).toBe(-15);
+                    });
+                    it('La duracion sera la media de duraciones de los efectos de cada ingrediente, redondeada para abajo', async () => {
+    
+                        const ingredients = await getIngredients();
+                        const diseases = await getDiseases();
+    
+                        const selectedIngredients = [getSelectedIngredient(ingredients, "Tranquil Leaf"), getSelectedIngredient(ingredients, "Tranquil Leaf")];
+    
+                        const elixir = PotionFactory.createPotion(selectedIngredients, diseases);
+    
+                        expect(elixir.duration).toBe(2);
+                    });
+                });
+                describe('Cuando todos los efectos son del tipo greater', () => {
+                    it('El valor resultante del atributo sera la media de los values de los ingredientes. Una vez calculada la media se redondeara al multiplo de 5 inferior', async () => {
+    
+                        const ingredients = await getIngredients();
+                        const diseases = await getDiseases();
+    
+                        const selectedIngredients = [getSelectedIngredient(ingredients, "Serenity Blossom"), getSelectedIngredient(ingredients, "Serenity Blossom")];
+    
+                        const elixir = PotionFactory.createPotion(selectedIngredients, diseases);
+                        
+                        expect(elixir.modifiers.charisma).toBe(-20);
+                    });
+                    it('La duracion sera la media de duraciones de los efectos de cada ingrediente, redondeada para abajo', async () => {
+    
+                        const ingredients = await getIngredients();
+                        const diseases = await getDiseases();
+    
+                        const selectedIngredients = [getSelectedIngredient(ingredients, "Serenity Blossom"), getSelectedIngredient(ingredients, "Serenity Blossom")];
+    
+                        const elixir = PotionFactory.createPotion(selectedIngredients, diseases);
+    
+                        expect(elixir.duration).toBe(3);
+                    });
+                });
+                describe('Cuando todos los efectos son de tipo diferente', () => {
+                    it('El valor resultante del atributo sera la media de los values de los ingredientes. Una vez calculada la media se redondeara al multiplo de 5 inferior', async () => {
+    
+                        const ingredients = await getIngredients();
+                        const diseases = await getDiseases();
+    
+                        const selectedIngredients = [getSelectedIngredient(ingredients, "Serenity Blossom"), getSelectedIngredient(ingredients, "Quieting Root")];
+    
+                        const elixir = PotionFactory.createPotion(selectedIngredients, diseases);
+                        
+                        expect(elixir.modifiers.charisma).toBe(-10);
+                    });
+                    it('La duracion sera la media de duraciones de los efectos de cada ingrediente, redondeada para abajo', async () => {
+    
+                        const ingredients = await getIngredients();
+                        const diseases = await getDiseases();
+    
+                        const selectedIngredients = [getSelectedIngredient(ingredients, "Serenity Blossom"), getSelectedIngredient(ingredients, "Quieting Root")];
+    
+                        const elixir = PotionFactory.createPotion(selectedIngredients, diseases);
+    
+                        expect(elixir.duration).toBe(2);
+                    });
+                });
+                it('El nombre de la pocion sera: Modifier + Attribute + Elixir. El modificador del nombre sera el que corresponda con el value correspondiente segun la tabla', async () => {
+    
+                    const ingredients = await getIngredients();
+                    const diseases = await getDiseases();
+
+                    const selectedIngredients = [getSelectedIngredient(ingredients, "Serenity Blossom"), getSelectedIngredient(ingredients, "Quieting Root")];
+
+                    const elixir = PotionFactory.createPotion(selectedIngredients, diseases);
+
+                    expect(elixir.name).toBe('Lesser Calm Elixir');
+                });
+            });
+        });
+    });
+    describe('Cuando alguno de los efectos de ingredientes no lleva el nombre "Calm" o "Boost"', () => {
+        it('No podremos crear el elixir. (Comprobar nombre)', async () => {
+    
+            const ingredients = await getIngredients();
+            const diseases = await getDiseases();
+
+            const selectedIngredients = [getSelectedIngredient(ingredients, "Guardian's Essence"), getSelectedIngredient(ingredients, "Defenseless Herb")];
+            
+            const elixir = PotionFactory.createPotion(selectedIngredients, diseases);
+
+            expect(elixir.name).not.toContain('Elixir');
+        });
+    });
 });
