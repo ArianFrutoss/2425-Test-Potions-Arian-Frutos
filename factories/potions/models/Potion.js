bfbcc2e2d4d94707f3ff3b12bcef9879b1ad2aed
ArianFrutoss <arian.frutos@ikasle.aeg.eus>
2025-01-14 08:27:41 +0100

    ADD - the potions creator files

---
+++ factories/potions/models/Potion.js
@@ -0,0 +1,20 @@
+import Modifiers from '../../../interfaces/Modifiers';
+
+export class Potion {
+    name;
+    type;
+    modifiers;
+  
+    constructor(name = '', type = '', modifiers = { 
+        intelligence: 0, 
+        dexterity: 0, 
+        constitution: 0, 
+        insanity: 0, 
+        charisma: 0, 
+        strength: 0 
+      }) {
+      this.modifiers = modifiers;
+      this.name = name;
+      this.type = type;
+    }
+  }
