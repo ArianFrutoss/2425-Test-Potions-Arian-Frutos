6c12a149579d0eb3de5d140a0745b515131c8b86
ArianFrutoss <arian.frutos@ikasle.aeg.eus>
2025-01-14 11:41:22 +0100

    ADD - a test for the normal elixires

--- test/elixir.test.js
+++ test/elixir.test.js
@@ -52,6 +52,30 @@ describe('Cuando el numero de ingredientes es 2-4', () => {
                         expect(elixir.duration).toBe(1);
                     });
                 });
+                describe('Cuando todos los efectos son del tipo normal', () => {
+                    it('El valor resultante del atributo sera la media de los values de los ingredientes. Una vez calculada la media se redondeara al multiplo de 5 inferior', async () => {
+    
+                        const ingredients = await getIngredients();
+                        const diseases = await getDiseases();
+    
+                        const selectedIngredients = [getSelectedIngredient(ingredients, "Dreamer's Dew"), getSelectedIngredient(ingredients, "Dreamer's Dew")];
+    
+                        const elixir = PotionFactory.createPotion(selectedIngredients, diseases);
+                        
+                        expect(elixir.modifiers.charisma).toBe(15);
+                    });
+                    it('La duracion sera la media de duraciones de los efectos de cada ingrediente, redondeada para abajo', async () => {
+    
+                        const ingredients = await getIngredients();
+                        const diseases = await getDiseases();
+    
+                        const selectedIngredients = [getSelectedIngredient(ingredients, "Dreamer's Dew"), getSelectedIngredient(ingredients, "Dreamer's Dew")];
+    
+                        const elixir = PotionFactory.createPotion(selectedIngredients, diseases);
+    
+                        expect(elixir.duration).toBe(2);
+                    });
+                });
             });
         });
     });
