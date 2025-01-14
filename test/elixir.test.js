c5fab6bb09165cb40d5507a03f266ae8ece2a319
ArianFrutoss <arian.frutos@ikasle.aeg.eus>
2025-01-14 11:30:57 +0100

    ADD - a test for the least elixires

---
+++ test/elixir.test.js
@@ -0,0 +1,34 @@
+import { PotionFactory } from "../factories/potions/PotionFactory";
+import { getDiseases, getIngredients } from "../getIngredientAndDiseases";
+import { getSelectedIngredient } from "../getSelectedIngredientsAndDiseases";
+
+describe('Cuando el numero de ingredientes es 2-4', () => {
+    describe('Cuando los efectos de ingredientes asociados llevaran los nombres "Boost"', () => {
+        describe('Cuando todos los ingredientes tienen el mismo atributo (INT, DEX...)', () => {
+            describe('Cuando todos los efectos son del tipo least', () => {
+                it('El valor resultante del atributo sera la media de los values de los ingredientes. Una vez calculada la media se redondeara al multiplo de 5 inferior', async () => {
+
+                    const ingredients = await getIngredients();
+                    const diseases = await getDiseases();
+
+                    const selectedIngredients = [getSelectedIngredient(ingredients, "Radiant Petal"), getSelectedIngredient(ingredients, "Radiant Petal")];
+
+                    const elixir = PotionFactory.createPotion(selectedIngredients, diseases);
+
+                    expect(elixir.modifiers.charisma).toBe(5);
+                });
+                it('La duracion sera la media de duraciones de los efectos de cada ingrediente, redondeada para abajo', async () => {
+
+                    const ingredients = await getIngredients();
+                    const diseases = await getDiseases();
+
+                    const selectedIngredients = [getSelectedIngredient(ingredients, "Radiant Petal"), getSelectedIngredient(ingredients, "Radiant Petal")];
+
+                    const elixir = PotionFactory.createPotion(selectedIngredients, diseases);
+
+                    expect(elixir.duration).toBe(1);
+                });
+            });
+        });
+    });
+});
