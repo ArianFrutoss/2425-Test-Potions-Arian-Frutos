304af55c1134ad9d7af8ce575e2a1f5e7f5fb1fe
ArianFrutoss <arian.frutos@ikasle.aeg.eus>
2025-01-14 11:35:54 +0100

    ADD - a test for the least elixires

--- test/elixir.test.js
+++ test/elixir.test.js
@@ -28,6 +28,30 @@ describe('Cuando el numero de ingredientes es 2-4', () => {
 
                     expect(elixir.duration).toBe(1);
                 });
+                describe('Cuando todos los efectos son del tipo lesser', () => {
+                    it('El valor resultante del atributo sera la media de los values de los ingredientes. Una vez calculada la media se redondeara al multiplo de 5 inferior', async () => {
+    
+                        const ingredients = await getIngredients();
+                        const diseases = await getDiseases();
+    
+                        const selectedIngredients = [getSelectedIngredient(ingredients, "Sageleaf"), getSelectedIngredient(ingredients, "Sageleaf")];
+    
+                        const elixir = PotionFactory.createPotion(selectedIngredients, diseases);
+    
+                        expect(elixir.modifiers.charisma).toBe(10);
+                    });
+                    it('La duracion sera la media de duraciones de los efectos de cada ingrediente, redondeada para abajo', async () => {
+    
+                        const ingredients = await getIngredients();
+                        const diseases = await getDiseases();
+    
+                        const selectedIngredients = [getSelectedIngredient(ingredients, "Sageleaf"), getSelectedIngredient(ingredients, "Sageleaf")];
+    
+                        const elixir = PotionFactory.createPotion(selectedIngredients, diseases);
+    
+                        expect(elixir.duration).toBe(1);
+                    });
+                });
             });
         });
     });
